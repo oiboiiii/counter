42f459975c8c9e5e8b94706b69127b28bcc7f363
oiboiiii <toma.ttora@gmail.com>
2025-02-23 22:50:42 +0300

    adds counter

---
+++ src/components/Сounter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(prevCount => prevCount + 1);
+    };
+
+    return <button onClick={handleClick}>{count}</button>
+}
+
+export default Counter;
